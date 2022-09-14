6f593ba08fe800e512d5ad558499454ba0605615
ddm <7736453+dongdongmao@users.noreply.github.com>
2022-09-15 00:21:49 +0800

    code temp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'excel-editor';
+  constructor() {}
+
+  uploadFile(e: any) {
+    const file = e.target.files[0] as File;
+    const buffer = file.arrayBuffer() as Promise<any>;
+    const name = file.name;
+
+    buffer.then((result) => {
+      const wb = XLSX.read(result);
+      let sheetNames = wb.SheetNames;
+      sheetNames.forEach((n) => {
+        let json = XLSX.utils.sheet_to_json(wb.Sheets[n]) as Array<any>;
+        wb.Sheets[n]['!cols'];
+        json.forEach((row) => {
+          let title = row['Title'];
+          title += ' /';
+          row['Title'] = title;
+        });
+        let changedSheet = XLSX.utils.json_to_sheet(json);
+        wb.Sheets[n] = changedSheet;
+      });
+      XLSX.writeFile(wb, name);
+    });
+  }
 }
